56be5121042331f1b6fde949b335ebd9cc828270
sheikhabdullah.zhd <sheikhabdullah.zhd@gmail.com>
2025-05-06 17:43:07 +0000

    Fix some changes

--- client/src/components/shared/Logo.tsx
+++ client/src/components/shared/Logo.tsx
@@ -1,24 +1,9 @@
-import React from "react";
+import React from 'react';
+
+import logo from '../../../attached_assets/generated-icon.png'
 
 export const Logo: React.FC<{ className?: string }> = ({ className = "" }) => {
-  return (
-    <svg 
-      width="100" height="100" viewBox="0 0 100 100" 
-      fill="none" 
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-    >
-      <rect width="100" height="100" fill="black" />
-      <g transform="translate(20,20)">
-        <rect x="5" width="50" height="7" rx="3.5" fill="#63B1E7"/>
-        <rect x="5" y="12" width="40" height="7" rx="3.5" fill="#63B1E7"/>
-        <rect x="5" y="24" width="30" height="7" rx="3.5" fill="#63B1E7"/>
-        <rect x="5" y="36" width="45" height="7" rx="3.5" fill="#63B1E7"/>
-        <rect x="5" y="48" width="55" height="7" rx="3.5" fill="#63B1E7"/>
-        <rect x="5" y="60" width="40" height="7" rx="3.5" fill="#63B1E7"/>
-      </g>
-    </svg>
-  );
+  return <img src={logo} className={className} alt="logo"/>;
 };
 
 export default Logo;
